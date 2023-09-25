4810555e1f902a62a88266b1655cd59a889b7516
YssfDevOps <youssef.cah.7912@gmail.com>
2023-09-25 18:27:16 +0200

    main V2

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ import seaborn as sns
 import numpy as np
 import pandas as pd
 import math
+import string
 
 def load_puzzle_crossword(filename):
     with open(filename, 'r') as fileCW:
         # Obtain file content by rows.
         CW = fileCW.readlines()
-
     # Load text file into a NumPy matrix.
     crossword = []
     for row in CW:
@@ -20,16 +20,49 @@ def load_puzzle_crossword(filename):
     return np.matrix(crossword)
 
 def load_dictionary(filename):
+    dictionary = []
+    with open(filename, 'r') as fileDict:
+        for line in fileDict:
+            dictionary.append(line.strip())
+    return dictionary
+
+def satisfy_restriccions(assignation, LVA, R):
     pass
 
 def backtracking(LVA, LVNA, R, D):
-    pass
+    if not LVNA:
+        return LVA
 
-def satisfy_restriccions(asignation, LVA, R):
-    pass
+    Var = LVNA[0]
+
+    for valor in D:
+        if satisfy_restriccions(valor, LVA, R):
+            # TODO: Make Insertar y Cua
+
+
+            Res = backtracking()
+            if Res is not None:
+                return Res
+
+    return None
 
 if __name__ == '__main__':
+
+    # Structures creation.
+    LVA = [[]]
     LVNA = load_puzzle_crossword("crossword_CB_v3.txt")
-    print(LVNA)
+
+    # Load up files.
+    dictionary = load_dictionary("diccionari_CB_v3.txt")
+    alphabet = list(string.ascii_uppercase)
+
+    # Function call.
+    res = backtracking(LVA, LVNA, dictionary, alphabet)
+
+    if res is not None:
+        print(res)
+    else:
+        print("Incorrect result.")
+
 
 
