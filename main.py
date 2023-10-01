1f49f147e879cdd0e4d10fe31722d7143a6ce1e7
YssfDevOps <youssef.cah.7912@gmail.com>
2023-10-01 19:49:04 +0200

    Fix of some functions and optimizations

--- main.py
+++ main.py
@@ -8,117 +8,8 @@ import pandas as pd
 import math
 import string
 
-D = [
-    "ACATAR",
-    "AFERRISSAU",
-    "AGUISSAVEU",
-    "ALLOFONIES",
-    "ALTA",
-    "AMORTASSEN",
-    "ANYOCASSIN",
-    "APROFITASSEU",
-    "ARRENGARA",
-    "ASSOCIESSEM",
-    "AUTENTIFICARIES",
-    "BANALMENT",
-    "BESCOLLEJAREN",
-    "BORE",
-    "BUFALAGA",
-    "CARA",
-    "CARNET",
-    "CENTRACIONS",
-    "CLAN",
-    "COMPAGIN",
-    "CONFORTESSIU",
-    "CONTRAGIRAU",
-    "COR",
-    "CUPULAR",
-    "DEJUNES",
-    "DESAGERMANESSIN",
-    "DESBOQUESSIM",
-    "DESCOVAREM",
-    "DESEMPASTASSIN",
-    "DESENFETGEGAVA",
-    "DESENVALISESSIN",
-    "DESINFLAMEU",
-    "DESPARAVA",
-    "DESUNGLAS",
-    "DIARI",
-    "DIEM",
-    "DO",
-    "DORAT",
-    "ELUIA",
-    "EMBOVAVEU",
-    "EMPANTANEGARA",
-    "EMPRESONAU",
-    "ENCASTESSIS",
-    "ENDANYESSIS",
-    "ENFREIXURARIA",
-    "ENGUERXINASSEN",
-    "ENRINXAVEU",
-    "ENTERANYINESSIN",
-    "ENUCLEESSEN",
-    "ESBALAIXEN",
-    "ESCAMOTEJARIEM",
-    "ESCORNIFLAREM",
-    "ESGRUMESSEU",
-    "ESPENYADA",
-    "ESQUERRARAS",
-    "ESTRANGERITZANT",
-    "EXCULPAREN",
-    "FALDILLER",
-    "FILTRAU",
-    "FORFOLLAU",
-    "FURGUESSEU",
-    "GELATINITZASSIU",
-    "GRATIFICAREN",
-    "HEROISME",
-    "IMMOLARES",
-    "INEDUCABLES",
-    "INTERFERIRIES",
-    "JUGO",
-    "LA",
-    "MADRASSA",
-    "MANS",
-    "MENTISSIS",
-    "MI",
-    "MOBLIN",
-    "MURMURI",
-    "NOSTOCALS",
-    "ODI",
-    "ON",
-    "PA",
-    "PEDREGADES",
-    "PI",
-    "PIN",
-    "PLANTOFES",
-    "POSICIONASSIU",
-    "PREMI",
-    "PUDELASSIU",
-    "RA",
-    "RANCI",
-    "RE",
-    "RECENTMENT",
-    "REESMERÇADA",
-    "REJUNTESSIN",
-    "REPLANTARIEN",
-    "RETINGUEN",
-    "ROBOTITZADA",
-    "SALESSEN",
-    "SEMPITERNITAT",
-    "SOBREEIXIRIES",
-    "SORRAMOLLS",
-    "SUPERARIEM",
-    "TALLER",
-    "TOTAL",
-    "TRANSVASARAS",
-    "TRONXON",
-    "VANAGLORIESSIN",
-    "VISA",
-    "XIPOLLESSIM"
-]
 # VARIABLES GLOBALES
-dim = [0,0]         # DIMENSIONES DEL TABLERO
+dim = [0, 0]        # DIMENSIONES DEL TABLERO
 board = []          # TABLERO
 boardAux = []
 LVA = {}            # LLISTA VALORS ASSIGNATS
@@ -126,44 +17,49 @@ LVNA = []           # LLISTA VALORS NO ASSIGNATS
 HORIZONTAL = '0'    # CODIGO PARA PALABRA HORIZONTAL
 VERTICAL = '1'      # CODIGO PARA PALABRA VERTICAL
 
-def horizontal(lista, cap):
-    for fila in range(dim[0]):
-        for columna in range(dim[1]):
-            if board[fila][columna] == '0' and len(lista) < dim[0]:
-                if cap == ['','','', 0]: #guardar l'inici de les paraules
-                    cap = [fila, columna, HORIZONTAL, 0]
-                lista.append('0')
-            else:
-                if len(lista) > 1: cap[3]=len(lista); LVNA.append(cap.copy())
-                lista.clear()
-                cap = ['','','', 0]
-        
-        if len(lista) > 1: cap[3]=len(lista); LVNA.append(cap.copy())
-        lista.clear()
-        cap = ['','','', 0]
 
-def vertical(lista, cap):
-    for columna in range(dim[1]):
-        for fila in range(dim[0]):
-            if board[fila][columna] == '0' and len(lista) < dim[1]:
-                if cap == ['','','', 0]: #guardar l'inici de les paraules
-                    cap = [fila, columna, VERTICAL, 0]
+def horizontal():
+    cap = ['', '', '', 0]  # fila , columna, direccion, longitud
+    for fila in range(dim[0]):
+        lista = []
+        cap = [fila, 0, HORIZONTAL, 0]
+        for columna in range(dim[1]):
+            if board[fila][columna] == '0':
                 lista.append('0')
             else:
-                if len(lista) > 1: cap[3]=len(lista); LVNA.append(cap.copy())
+                if len(lista) > 1:
+                    cap[3] = len(lista)
+                    LVNA.append(cap.copy())
                 lista.clear()
-                cap = ['','','', 0]
-                
-        if len(lista) > 1: cap[3]=len(lista); LVNA.append(cap.copy())
-        lista.clear()
-        cap = ['','','', 0]
+                cap = [fila, columna + 1, HORIZONTAL, 0]
+        if len(lista) > 1:
+            cap[3] = len(lista)
+            LVNA.append(cap.copy())
+
+
+def vertical():
+    cap = ['', '', '', 0]  # fila , columna, direccion, longitud
+    for columna in range(dim[1]):
+        lista = []
+        cap = [0, columna, VERTICAL, 0]
+        for fila in range(dim[0]):
+            if board[fila][columna] == '0':
+                lista.append('0')
+            else:
+                if len(lista) > 1:
+                    cap[3] = len(lista)
+                    LVNA.append(cap.copy())
+                lista.clear()
+                cap = [fila + 1, columna, VERTICAL, 0]
+        if len(lista) > 1:
+            cap[3] = len(lista)
+            LVNA.append(cap.copy())
+
 
 def load_LVNA():
-    lista = []
-    cap = ['','','', 0] #fila , columna, direccion, longitud
-    # TODO: poner longitud
-    horizontal(lista, cap)
-    vertical(lista, cap)
+    horizontal()
+    vertical()
+
 
 def load_puzzle_crossword(filename):
     with open(filename, 'r') as fileCW:
@@ -174,7 +70,8 @@ def load_puzzle_crossword(filename):
         row = [str(square) for square in row.strip().split('\t')]
         board.append(row)
         dim[0] = len(board)
-        dim[1] = len(board[0]) #guardamos la dimension
+        dim[1] = len(board[0])  # Guardamos la dimension
+
 
 def load_dictionary(filename):
     dictionary = []
@@ -183,16 +80,19 @@ def load_dictionary(filename):
             dictionary.append(line.strip())
     return dictionary
 
+
 def satisfy_restriccions(assignWord, Var):
     # Coincidan las longitudes
-    if len(assignWord) == Var[3]: 
-        #Mirar si coinciden letras
+    if len(assignWord) == Var[3]:
+        # Mirar si coinciden letras
         fila = int(Var[0])
         columna = int(Var[1])
         direccion = Var[2]
         hueco = []
-        
+
         for i in range(Var[3]):
+            if board[fila][columna] == '#':
+                return False
             hueco.append(board[fila][columna])
             if direccion == HORIZONTAL:
                 columna = columna + 1
@@ -207,10 +107,7 @@ def satisfy_restriccions(assignWord, Var):
 
 def backtracking(LVA, LVNA, D):
     if not LVNA: return LVA
-    boardAux.clear()
-    if len(board) > 0: #COMO IMPLEMENTAR ESTO
-        for fila in board:
-            boardAux.append(fila.copy())
+    boardAux = [fila.copy() for fila in board]
     Var = LVNA[0]
     for assignWord in D:
         if satisfy_restriccions(assignWord, Var):
@@ -221,33 +118,32 @@ def backtracking(LVA, LVNA, D):
                 return Res
             else:
                 board.clear()
-                for fila in boardAux:
-                    board = fila.copy()
+                board.extend(boardAux)
     return None
 
 def update_board(Var, assignWord):
-    fila = Var[0]
-    columna = Var[1]
-    for i in range(Var[3]):
+    fila = int(Var[0])
+    columna = int(Var[1])
+    for i in range(int(Var[3])):
         board[fila][columna] = assignWord[i]
         if Var[2] == HORIZONTAL:
-            columna = columna + 1
+            columna += 1
         else:
-            fila = fila + 1
+            fila += 1
 
 if __name__ == '__main__':
 
     # Carga de tablero y diccionario.
-    load_puzzle_crossword("crossword_CB_v3.txt") 
+    load_puzzle_crossword("crossword_CB_v3.txt")
     load_LVNA()
 
     # TODO Arreglar diccionario.
-    # dictionary = load_dictionary("diccionari_CB_v3.txt")
-    
+    dictionary = load_dictionary("diccionari_CB_v3.txt")
+
     # Function call.
-    res = backtracking(LVA, LVNA, D)
+    res = backtracking(LVA, LVNA, dictionary)
 
     if res is not None:
-      print(res)
+        print(res)
     else:
         print("Incorrect result.")
