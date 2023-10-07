095609ea6cc62c6076837877c722936d9b551285
jg03dev <jan.gra.ser.001@gmail.com>
2023-10-07 18:22:32 +0000

    Bugs fixed, Backtracking done

--- main.py
+++ main.py
@@ -7,14 +7,11 @@ from word import Word
 from copy import copy, deepcopy
 
 # VARIABLES GLOBALES
-dim = [0, 0]        # Dimensions del taulell
-board = []          # TABLERO
-LVA = []            # LLISTA VALORS ASSIGNATS
-LVNA = []           # LLISTA VALORS NO ASSIGNATS
 HORIZONTAL = 0      # CODIGO PARA PALABRA HORIZONTAL
 VERTICAL = 1        # CODIGO PARA PALABRA VERTICAL
 
-def horizontal():
+
+def horizontal(board, dim, LVNA):
     for fila in range(dim[0]):
         lista = []
         for columna in range(dim[1]):
@@ -23,22 +20,21 @@ def horizontal():
             else:
                 if len(lista) > 1:
                     w = Word(s=[fila, columna - len(lista)], l=len(lista), o=HORIZONTAL)
-                    LVNA.append(deepcopy(w))
+                    LVNA.append(w)
                 lista.clear()
         if len(lista) > 1:
             w = Word(s=[fila, dim[1] - len(lista)], l=len(lista), o=HORIZONTAL)
-            LVNA.append(deepcopy(w))
+            LVNA.append(w)
 
 
-            
-def mirar_veins(casella, lw, n_horizontals):
+def mirar_veins(casella, lw, n_horizontals, LVNA):
     for Hw in LVNA[:n_horizontals]:
         if ((Hw.pertany([casella[0], casella[1]+1]) or Hw.pertany([casella[0], casella[1]-1]))
                 and Hw not in lw):
             lw.append([Hw, casella])
 
 
-def vertical():
+def vertical(board, dim, LVNA):
     n_horizontals = len(LVNA)
     for columna in range(dim[1]):
         lista = []
@@ -46,26 +42,26 @@ def vertical():
         for fila in range(dim[0]):
             if board[fila][columna] == '0':
                 lista.append('0')
-                mirar_veins([fila, columna], lw, n_horizontals)
+                mirar_veins([fila, columna], lw, n_horizontals, LVNA)
             else:
                 if len(lista) > 1:
                     w = Word(s=[fila - len(lista), columna], l=len(lista), o=VERTICAL, lw=lw)
                     w.update_linked()
-                    LVNA.append(deepcopy(w))
+                    LVNA.append(w)
                 lista.clear()
                 lw.clear()
         if len(lista) > 1:
             w = Word(s=[dim[0] - len(lista), columna], l=len(lista), o=VERTICAL, lw=lw)
             w.update_linked()
-            LVNA.append(deepcopy(w))
+            LVNA.append(w)
 
 
-def load_LVNA():
-    horizontal()
-    vertical()
+def load_LVNA(board, dim ,LVNA):
+    horizontal(board, dim, LVNA)
+    vertical(board, dim, LVNA)
 
 
-def load_puzzle_crossword(filename):
+def load_puzzle_crossword(filename, board, dim):
     with open(filename, 'r') as fileCW:
         # Obtain file content by rows.
         CW = fileCW.readlines()
@@ -77,6 +73,7 @@ def load_puzzle_crossword(filename):
         dim[1] = len(board[0])  # Guardamos la dimension
 
 
+#TODO: Ordenar por longitud
 def load_dictionary(filename):
     dictionary = []
     with open(filename, 'r', encoding='ISO-8859-1') as fileDict:
@@ -86,64 +83,72 @@ def load_dictionary(filename):
 
 
 def satisfy_restriccions(assignWord, Var):
+    if len(assignWord) != Var.length:
+        return False
 
-    if len(assignWord) == Var.length:
-        posFila = Var.start[0]
-        posCol = Var.start[1]
-        direction = Var.orientation
+    posFila = Var.start[0]
+    posCol = Var.start[1]
+    direction = Var.orientation
+    for w, c in Var.linked_words:  # start[1] - c[1]
+        if w.value != "":  # Lo mismo que no assignado
+            if direction == HORIZONTAL:
+                if assignWord[c[1] - posCol] != w.value[c[0] - w.start[0]]:
+                    return False
+            else:  # Vertical
+                if assignWord[c[0] - posFila] != w.value[c[1] - w.start[1]]:
+                    return False
+    return True
 
-        for w, c in Var.linked_words:  # start[1] - c[1]
-            if w.value != "":  # Lo mismo que no assignado
-                if direction == HORIZONTAL:
-                    if assignWord[c[0] - posFila] != w.value[c[1] - posCol]:
-                        return False
-                else:
-                    if assignWord[c[0] - posFila] != w.value[c[1] - posCol]:
-                        return False
-        return True
-
-    return False
 
 def backtracking(LVA, LVNA, D):
-    if not LVNA: return LVA
+    if not LVNA:
+        return LVA
+
     Var = LVNA[0]  # Guardem el cap.
+
     for assignWord in D:
         if satisfy_restriccions(assignWord, Var):
             Var.value = assignWord
-            LVA.append(Var)
-            Res = backtracking(LVA, LVNA[1:], D)
+            Res = backtracking(LVA + [Var], LVNA[1:], D)
             if Res is not None:
                 return Res
+
+    # Si arribem aqui el valor de LVA deixa de tenir una assignació
     if LVA:
-        LVA.pop()
+        LVA[-1].value = ""
     return None
 
 
-# TODO MODIFICAR UPDATE_BOARD
-def update_board():
-    for w in LVA:
+def update_board(board, res):
+    for w in res:
         if w.orientation == HORIZONTAL:
             for i in range(w.length):
-                board[w.start[0]][w.start[1]+i] = w.value[i]
+                board[w.start[0]][w.start[1] + i] = w.value[i]
         else:
             for j in range(w.length):
-                board[w.start[0]+j][w.start[1]] = w.value[j]
+                board[w.start[0] + j][w.start[1]] = w.value[j]
     return board
 
 
 if __name__ == '__main__':
-    # Carga de tablero y diccionario.
-    load_puzzle_crossword("crossword_CB_v3.txt")
-    load_LVNA()
+    dim = [0, 0]  # Dimensions del taulell
+    board = []  # TABLERO
+    LVA = []  # LLISTA VALORS ASSIGNATS
+    LVNA = []  # LLISTA VALORS NO ASSIGNATS
+
+
+    # Carga de tablero y diccionario
+    load_puzzle_crossword("crossword_CB_v3.txt", board, dim)
+    load_LVNA(board, dim, LVNA)
     dictionary = load_dictionary("diccionari_CB_v3.txt")
 
     # Function call.
     res = backtracking(LVA, LVNA, dictionary)
 
-    print(update_board())
-
     if res is not None:
+        update_board(board, res)
+        for i in board:
+            print(*i)
         print([w.value for w in res])
     else:
         print("Incorrect result.")
-
--- word.py
+++ word.py
@@ -11,16 +11,17 @@ class Word:
     linked_words = []  # TODO mirar d'utilitzar numpy per aquesta estructura
     value = ""
 
-    def __init__(self, s=0, l=0, o=0, lw=[]):
+    def __init__(self, s, l, o, lw=None):
         self.start = s
         self.length = l
         self.orientation = o
-        self.linked_words = lw
-        return
+        self.linked_words = copy(lw) if lw is not None else []
+        self.value = ""
 
     def pertany(self, casella):
         return ((self.orientation == 0 and self.start[1] <= casella[1] <= self.start[1] + self.length and self.start[0] == casella[0])
                     or (self.orientation == 1 and self.start[0] <= casella[0] <= self.start[0] + self.length and self.start[1] == casella[1]))
+
     def update_linked(self):
         for w, c in self.linked_words:
             w.linked_words.append([self, c])
